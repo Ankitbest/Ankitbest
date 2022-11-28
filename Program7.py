7ec1c04a91aa92de207ee792f17b29c6db9b0fc7
Ankitbest <113026136+Ankitbest@users.noreply.github.com>
2022-11-28 12:53:41 +0530

    Simple Interest

---
+++ Program7.py
@@ -0,0 +1,10 @@
+Amount=int(input("ENTRE THE AMOUNT: "))
+
+Year=int(input("ENTRE THE YEAR : "))
+
+Rate=float(input("ENTRE THE RATE OF INTEREST: "))
+
+SimpleInterset=(Amount*Year*Rate)/100  
+
+print("THE SIMPLE INTEREST IS : ",SimpleInterset)
+
