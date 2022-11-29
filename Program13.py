0a324231c81970a1784e2e595cf7f4dea244ce5f
Ankitbest <113026136+Ankitbest@users.noreply.github.com>
2022-11-29 18:38:21 +0530

    Vowel Or Consonant

---
+++ Program13.py
@@ -0,0 +1,6 @@
+ch=input(" ENTRE A CHARACTER: ")
+if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+  print(ch," IS A VOWEL ") 
+else:
+  print(ch," IS A CONSONANT ") 
+  
