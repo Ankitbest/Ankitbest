c9edc721750d2e550ac7da28ca45bde0d012d46f
Ankitbest <113026136+Ankitbest@users.noreply.github.com>
2022-12-03 17:06:43 +0530

    Count Alphabet , digit & Special Character

---
+++ Program20.py
@@ -0,0 +1,15 @@
+string = input("PLEASE ENTER YOUR OWN STRING : ")
+alphabets = digits = special = 0
+
+for i in range(len(string)):
+    if(string[i].isalpha()):
+        alphabets = alphabets + 1
+    elif(string[i].isdigit()):
+        digits = digits + 1
+    else:
+        special = special + 1
+        
+print("\nTotal Number of Alphabets in this String :  ", alphabets)
+print("Total Number of Digits in this String :  ", digits)
+print("Total Number of Special Characters in this String :  ", special)
+  
