a6d202b538fecc240ffd039ecf06855992561bad
Ankitbest <113026136+Ankitbest@users.noreply.github.com>
2022-12-05 11:51:26 +0530

    Arera And Circumference Of Circle

---
+++ Program25.py
@@ -0,0 +1,7 @@
+PI = 3.14
+radius = float(input(' PLEASE ENTRE THE RADIUS OF CIRCLE: '))
+area = PI * radius * radius
+circumference = 2 * PI * radius
+
+print(" Area Of a Circle = %.2f" %area)
+print(" CIRCUMFERENCE OF CIRCLE= %.2f" %circumference)
