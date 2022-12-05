1968545dbd7a228cb69d0627756623919b58c405
Ankitbest <113026136+Ankitbest@users.noreply.github.com>
2022-12-05 12:04:44 +0530

    Arera Of Ractangle

---
+++ Program27.py
@@ -0,0 +1,9 @@
+width = float(input('PLEASE ENTRE THE WEIGHT OF A RECTANGLE : '))
+height = float(input('PLEASE ENTRE THE HEIGHT OF A RECTANGLE: '))
+
+Area = width * height
+
+Perimeter = 2 * (width + height)
+
+print("\n Area of a Rectangle is: %.2f" %Area)
+print(" Perimeter of Rectangle is: %.2f" %Perimeter)
