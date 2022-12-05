bb9807320f9498cf3d3dd550237bc5455099a365
Ankitbest <113026136+Ankitbest@users.noreply.github.com>
2022-12-05 13:47:58 +0530

    Square Star Pattern

---
+++ Program29.py
@@ -0,0 +1,8 @@
+side = int(input("PLEASE ENTER ANY SIDE OF A SQUARE  : "))
+
+print("SQUARE STAR PATTERN") 
+
+for i in range(side):
+    for i in range(side):
+        print('*', end = '  ')
+    print()
